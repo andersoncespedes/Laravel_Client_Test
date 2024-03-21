b284f7ab3ab180719f5e8c4e2d9274233036c480
anderson <andersontrabajos2@gmail.com>
2024-03-21 10:54:28 -0500

    fix: :bug: updating unitofwork because an error with the model country

--- app/UnitOfWork/UnitOfWork.php
+++ app/UnitOfWork/UnitOfWork.php
@@ -6,14 +6,14 @@ use App\Interface\IClientRepository;
 use App\Repositories\ClientRepository;
 use App\Repositories\CountryRepository;
 use App\Models\Client;
-use App\Models\Countries;
+use App\Models\Country;
 
 class UnitOfWork implements IUnitOfWork{
     private ICountryRepository $_country;
     private IClientRepository  $_client;
     private Client $_client_model;
-    private Countries $_countries_model;
-    public function __construct (Client $client, Countries $countries){
+    private Country $_countries_model;
+    public function __construct (Client $client, Country $countries){
         $this->_client_model = $client;
         $this->_countries_model = $countries;
     }
