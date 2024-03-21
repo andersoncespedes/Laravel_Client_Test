0bba70f631ed96cb24521cc6f0d9b812b78ca479
anderson <andersontrabajos2@gmail.com>
2024-03-20 22:24:29 -0500

    feat: :sparkles: Creation and migration of database

---
+++ database/migrations/2024_03_20_234922_create_clients_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string("name", 60);
+            $table->string("address", 255);
+            $table->string("phone", 50);
+            $table->unsignedBigInteger("id_country");
+            $table->foreign("id_country")->references("id")->on("countries");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
