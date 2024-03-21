233f29fe071783309f9c74714a446bb86c292913
anderson <andersontrabajos2@gmail.com>
2024-03-21 10:54:50 -0500

    feat: :sparkles: adding relations to models

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -4,10 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Client extends Model
 {
+    use HasFactory;
     protected $table = "clients";
     protected $fillable = ["name", "address", "phone", "id_country"];
-    use HasFactory;
+    protected $hidden = ["created_at", "updated_at"];
+    public function Countries() : BelongsTo{
+        return $this->belongsTo(Country::class, "id_country");
+    }
 }
