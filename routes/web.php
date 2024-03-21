05250a7516e8cc07dddb863c0ebe5555bb0b56db
anderson <andersontrabajos2@gmail.com>
2024-03-21 10:56:22 -0500

    feat: :sparkles: adding new routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientController;
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [ClientController::class, "show"])->name("client_show");;
+Route::prefix("Client")->group(function(){
+    Route::get("/Create", [ClientController::class, "create"])->name("client_create");
+    Route::post("/Save", [ClientController::class, "store"])->name("client_store");
+    Route::delete("/Delete", [ClientController::class, "destroy"])->name("client_destroy");
+
 });
-Route::get('/Client', [ClientController::class, "Show"]);
-Route::post("/Client", [ClientController::class, "Save"])->name("save");
+
