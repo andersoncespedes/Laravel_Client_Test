3464b9d719a55fddb2c26fea9b46c4239ac8ce89
anderson <andersontrabajos2@gmail.com>
2024-03-21 21:02:20 -0500

    feat: :sparkles: Adding new routes

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,8 @@ Route::get('/', [ClientController::class, "show"])->name("client_show");;
 Route::prefix("Client")->group(function(){
     Route::get("/Create", [ClientController::class, "create"])->name("client_create");
     Route::post("/Save", [ClientController::class, "store"])->name("client_store");
-    Route::delete("/Delete", [ClientController::class, "destroy"])->name("client_destroy");
-
+    Route::delete("/Delete/{id}", [ClientController::class, "destroy"])->name("client_destroy");
+    Route::get("/Edit/{id}", [ClientController::class, "edit"])->name("client_edit");
+    Route::put("/Update/{id}", [ClientController::class, "update"])->name("client_update");
 });
 
