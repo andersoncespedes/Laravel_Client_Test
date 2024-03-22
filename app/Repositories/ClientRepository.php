33fcd49a944368a3feb25b58a45a5bb310e09b5e
anderson <andersontrabajos2@gmail.com>
2024-03-21 21:01:08 -0500

    perf: :zap: Improving the method GetWithCountries

--- app/Repositories/ClientRepository.php
+++ app/Repositories/ClientRepository.php
@@ -2,15 +2,14 @@
 namespace App\Repositories;
 use App\Interface\IClientRepository;
 use App\Models\Client;
-use Illuminate\Support\Collection;
-
+use Illuminate\Pagination\LengthAwarePaginator;
 class ClientRepository extends GenericRepository implements IClientRepository{
     private Client $_model;
     public function __construct(Client $model){
         $this->_model = $model;
         parent::__construct($this->_model);
     }
-    function getWithCountries() : Collection{
-        return $this->_model->with("countries")->get();
+    function getWithCountries() : LengthAwarePaginator{
+        return $this->_model->with("countries")->paginate(10);
     }
 }
