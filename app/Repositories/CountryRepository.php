1044c33e2cc0342c96fb51f8415dd006a35faa89
anderson <andersontrabajos2@gmail.com>
2024-03-23 14:36:55 -0500

    refactor: :zap: Updating the formate of the code

--- app/Repositories/CountryRepository.php
+++ app/Repositories/CountryRepository.php
@@ -2,13 +2,10 @@
 namespace App\Repositories;
 use App\Interface\ICountryRepository;
 use App\Models\Country;
-use Illuminate\Support\Collection;
-
 class CountryRepository extends GenericRepository implements ICountryRepository{
     private Country $_model;
     public function __construct(Country $model){
         $this->_model = $model;
         parent::__construct($this->_model);
     }
-  
 }
--- app/Repositories/GenericRepository.php
+++ app/Repositories/GenericRepository.php
@@ -8,11 +8,11 @@ class GenericRepository implements IGenericRepository
     public function __construct(object $model){
         $this->_model = $model;
     }
-    function save(array $data) : void
+    public function save(array $data) : void
     {
         $this->_model->create($data);
     }
-    function delete(int $id) : bool{
+    public function delete(int $id) : bool{
         $entity = $this->_model->findOrFail($id);
         if(is_null($entity) ){
             return false;
@@ -20,15 +20,15 @@ class GenericRepository implements IGenericRepository
         $entity->delete();
         return true;
     }
-    function update(int $id, array $data) : void
+    public function update(int $id, array $data) : void
     {
         $this->_model->where("id", $id)->update($data);
     }
-    function listAll() : Collection
+    public function listAll() : Collection
     {
         return $this->_model->all();
     }
-    function getOne(int $id) : object{
+    public function getOne(int $id) : object{
         return $this->_model->findOrFail($id);
     }
 }
